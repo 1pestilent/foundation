6e5a9eb280e631d24816598da04bb62d739a7005
Mikhail Ermakov <1pestilent@gmail.com>
2026-02-17 12:28:04 +0300

    обработка ошибки валидации

--- libs/foundation-web-starter/src/main/java/me/xpestilent/foundation/web/handler/GlobalExceptionHandler.java
+++ libs/foundation-web-starter/src/main/java/me/xpestilent/foundation/web/handler/GlobalExceptionHandler.java
@@ -8,9 +8,12 @@ import me.xpestilent.foundation.web.service.ExceptionLoggerService;
 import me.xpestilent.foundation.web.service.MetricService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 @Slf4j
@@ -41,6 +44,21 @@ public class GlobalExceptionHandler {
         metricService.ifPresent(service -> service.enrich(response, ex, ex.getStatus()));
 
         return ResponseEntity.status(ex.getStatus()).body(response);
+    }
+
+    @ExceptionHandler()
+    public ResponseEntity<ApiResponse<Void>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(error ->
+            errors.put(error.getField(), error.getDefaultMessage()));
+
+        ApiResponse<Void> response = ApiResponse.error("VALIDATION_ERROR", "Ошибка валидации");
+        response.getError().setDetails(errors);
+
+        metricService.ifPresent(service -> service.enrich(response, ex, HttpStatus.BAD_REQUEST));
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
