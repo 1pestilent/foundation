1953f1fb9365b76290b3c5c30548b860cb8f376f
Mikhail Ermakov <1pestilent@gmail.com>
2026-02-17 15:16:35 +0300

    исправил версию зависимости spring-doc (была для 4 спринга и не стартовала), добавил первоначальные сущности пользователя

--- services/user-service/user-service-impl/src/main/java/me/xpestilent/user/impl/UserServiceApplication.java
+++ services/user-service/user-service-impl/src/main/java/me/xpestilent/user/impl/UserServiceApplication.java
@@ -1,7 +1,13 @@
 package me.xpestilent.user.impl;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class UserServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceApplication.class, args);
+    }
 }
