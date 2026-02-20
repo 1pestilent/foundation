e1e8e04f151d3c942429ce2b2f08308363ddf0bf
Mikhail Ermakov <1pestilent@gmail.com>
2026-02-20 12:06:38 +0300

    created contracts for apis

--- services/user-service/user-service-api/src/main/java/me/xpestilent/user/api/client/InternalUserApi.java
+++ services/user-service/user-service-api/src/main/java/me/xpestilent/user/api/client/InternalUserApi.java
@@ -3,7 +3,6 @@ package me.xpestilent.user.api.client;
 import me.xpestilent.user.api.constant.UserApiPaths;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping(UserApiPaths.USERS_PUBLIC)
-public interface UserApi {
-
+@RequestMapping(UserApiPaths.USERS_INTERNAL)
+public interface InternalUserApi {
 }
